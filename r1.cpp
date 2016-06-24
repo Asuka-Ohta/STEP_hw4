b5589b725f3a151a651175d20fe6408b676e0938
Asuka-Ohta <ringochan0815@gmail.com>
2016-06-24 13:12:02 +0900

    Initialize repository

--- r1.cpp
+++ r1.cpp
@@ -4,31 +4,29 @@
 #include <map>
 #include <fstream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 typedef map<string, vector<int> > adj_matrix; //隣接行列
+typedef float number = 100.; // 各要素にはじめに与えられる値
 
 class Pagerank {
 private:
-  adj_matrix matrix; // 隣接行列
+  adj_matrix matrix; // リンクデータ
+  map<string, double> paderank;
+  map<string, double> temprank;
   int N; // 要素数
+
 public:
-  int set_N();
-  PageRank(int n, number alpha): n(n), a(n), alpha(alpha) {
+  PageRank(int n): 
+    N(n), matrix(n) {
     for (int i = 0; i < n; i++) initial.push_back(i);
   }
 };
-
-int Pagerank::set_N(int x) {
-  x > N;
-}
-  
+ 
 int main(){
   adj_matrix g; // 隣接行列
   adj_matrix::iterator gv;
-  vector<int> v; 
-  vector<string> w; // 要素の配列
-  float number = 100.; // 各要素にはじめに与えられる値
 
   string filename;
   cout << "ファイル名：";
@@ -41,6 +39,7 @@ int main(){
   }
   getline(ifs, str);
   int elements = atoi(str.c_str());
+  int p[elements];
   for (int i=0; i<elements; i++) {
     getline(ifs, str);
     w.push_back(str);
